57022effa4bae03814b66b0e339b8f51322b8a61
VadymChutur <104319186+VadymChutur@users.noreply.github.com>
2022-10-17 12:41:29 +0000

    Deploying to gh-pages from @ VadymChutur/goit-js-hw-10@76e3a7834eb52204ecc297d2a6f301be388b1944 🚀

--- index.59bab8b7.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/name/ukraine").then((o=>{if(!o)throw new Error(o.status);return o.json()})).then((o=>{!function(o){const{name:l,capital:n,population:e,flags:{svg:s},languages:c}=o[0];console.log(o[0]),console.log(o[0].name.official),console.log(o[0].capital[0]),console.log(o[0].population),console.log(o[0].flags.svg),console.log(o[0].languages),console.log(l),console.log(n),console.log(e),console.log(s),console.log(c[0])}(o)})).catch((o=>console.log(o)));
-//# sourceMappingURL=index.59bab8b7.js.map
